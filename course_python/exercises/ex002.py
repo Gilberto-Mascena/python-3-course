577704c38e4d78b7f3b213a390d4576e3726f10a
Gilberto Mascena <gilbertomascena@gmail.com>
2024-09-08 16:11:48 -0300

    add new file (ex002)

---
+++ course_python/exercises/ex002.py
@@ -0,0 +1,13 @@
+nome = "Ricardo"
+sobreome = "Silva"
+idade = 25
+ano_nascimento = 2024 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.75
+
+print('Nome:', nome)
+print('Sobrenome:', sobreome)
+print('Idade:', idade)
+print('Ano de Nascimento:', ano_nascimento)
+print('Maior de Idade:', maior_de_idade)
+print('Altura em Metros:', altura_metros)
