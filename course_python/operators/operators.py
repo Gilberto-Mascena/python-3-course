f813b8ccce13c41fc80bbfc01eaea85e463c759f
Gilberto Mascena <gilbertomascena@gmail.com>
2024-10-25 20:47:35 -0300

    add new folder

---
+++ course_python/operators/operators.py
@@ -0,0 +1,23 @@
+"""
+Operadores de comparação em Python sao usados para comparar valores (relacionais).
+Os operadores de comparação são:
+
+OPERADOR        DESCRIÇÃO         EXEMPLO
+
+== ............ Igual ........... 20 == 20
+!= ............ Diferente ....... 20 != 20
+> ............. Maior que ....... 20 > 20
+< ............. Menor que ....... 20 < 20
+>= ............ Maior ou igual .. 20 >= 20
+<= ............ Menor ou igual .. 20 <= 20
+
+Para usar o terminal interativo do Python, digite python3 -i  no terminal e pressione Enter.
+
+"""
+
+igual = 20 == 20
+diferente = 20 != 20
+maior = 20 > 27
+menor = 20 < 30
+maior_igual = 20 >= 20
+menor_igual = 20 <= 21
